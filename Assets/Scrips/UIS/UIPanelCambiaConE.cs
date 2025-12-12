dbc8cf3ffd4c830a1bd473ef903e1cc1aea2a940
1ndres005 <ndresfelipe2007@gmail.com>
2025-12-11 20:33:48 -0500

    ff

---
+++ Assets/Scrips/UIS/UIPanelCambiaConE.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class UIPanelCambiaConE : MonoBehaviour
+{
+    [Header("Pantallas de instrucciones en orden")]
+    public GameObject[] pantallas;
+
+    int indice = 0;
+    bool terminado = false;
+
+    void Start()
+    {
+        // Apagar todas menos la primera
+        for (int i = 0; i < pantallas.Length; i++)
+            pantallas[i].SetActive(i == 0);
+    }
+
+    void Update()
+    {
+        if (terminado)
+            return;
+
+        // Cambiar pantallas con E
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Apagar pantalla actual
+            pantallas[indice].SetActive(false);
+
+            indice++;
+
+            // Si llegó al final → cerrar todo
+            if (indice >= pantallas.Length)
+            {
+                terminado = true;
+                return;
+            }
+
+            // Activar la siguiente pantalla
+            pantallas[indice].SetActive(true);
+        }
+    }
+}
